45c0585ecf5dfea88ce6d97e7967d4d61dd9b1d1
Stud. Edoardo La Scala <e.lascala.0758@vallauri.net>
2020-10-23 13:04:19 +0200

    Lezione del 23-10-2020

--- 005-my-app/src/app/app.module.ts
+++ 005-my-app/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { ServerComponent } from './server/Server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 005-my-app/src/app/servers/servers.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Server } from 'tls';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-servers', //Coma tag
+  //selector: '[app-servers]', //Come attributo del tag in cui è inserito
+  //selector: '.app-servers',     //Come classe del tag in cui è inserito
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer:boolean = false; 
+  serverName:string = '- digita qui il nomde del tuo server -';
+  serverCreationStatus:string = 'No server was created!';
+  constructor() { 
+      setTimeout(() => {
+        this.allowNewServer = true; 
+      },2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server ' + this.serverName + ' was created!';
+    this.serverName = '- digita qui il nomde del tuo server -';
+  }
+
+  onUpdateServertName(event){
+    //console.lo
+    this.serverName = event.target.value;
+  }
+}
